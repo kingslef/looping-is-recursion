729d9cefa400add427fc6c3ccedc470f124f266d
Tuomas Lappeteläinen <tuomas.lappetelainen@gmail.com>
2017-05-03 22:43:40 +0300

    finish rest of the exercises

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -24,17 +24,52 @@
     :else false))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [i 0
+         s a-seq]
+    (cond
+      (empty? s) nil
+      (pred (first s)) i
+      :else (recur (inc i) (rest s)))))
 
 (defn avg [a-seq]
-  -1)
+  (loop [sum 0
+         count 0
+         s a-seq]
+    (if (empty? s)
+      (if (= count 0)
+        0
+        (/ sum count))
+      (recur (+ sum (first s)) (inc count) (rest s)))))
+
+;; From structured-data
+(defn toggle [a-set elem]
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn parity [a-seq]
-  ":(")
+  (loop [parities #{}
+         s a-seq]
+    (if (empty? s)
+      parities
+      (recur (toggle parities (first s)) (rest s)))))
 
 (defn fast-fibo [n]
-  ":(")
+  (if (= n 0)
+    0
+    ;; Not quite sure why this works..
+    (loop [f_n-1 1
+           f_n-2 0
+           n n]
+      (if (<= n 2)
+        (+ f_n-1 f_n-2)
+        (recur (+ f_n-1 f_n-2) f_n-1 (dec n))))))
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+  (loop [seen #{}
+         cut []
+         a-seq a-seq]
+    (if (or (empty? a-seq) (contains? seen (first a-seq)))
+      cut
+      (recur (conj seen (first a-seq)) (conj cut (first a-seq)) (rest a-seq)))))
 
